ccf34e7ebe36338cb0863a1287bbf67db01024e6
Andrew Walz <awalzpccam@gmail.com>
2025-12-15 10:26:25 +0100

    updated basic food type badge test to reflect appropriate strings

--- igp_doc_food_tracker/frontend/src/components/common/FoodTypeBadge.test.tsx
+++ igp_doc_food_tracker/frontend/src/components/common/FoodTypeBadge.test.tsx
@@ -3,7 +3,9 @@ import { render, screen } from "@testing-library/react";
 import FoodTypeBadge from "./FoodTypeBadge";
 
 test("renders English food type and shows tooltip", () => {
-  render(<FoodTypeBadge itType="salumi" engType="Meat Products" />);
+  render(
+    <FoodTypeBadge itType="Prodotti a base di carne" engType="Meat Products" />
+  );
 
-  expect(screen.getByText("Cured Meats")).toBeInTheDocument();
+  expect(screen.getByText("Meat Products")).toBeInTheDocument();
 });
